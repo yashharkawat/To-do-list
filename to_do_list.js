aba7b323303e16fb72c8c8f1d27ceb5088472077
Bikash Sahoo <Yashharkawat2000@gmail.com>
2023-07-11 17:02:13 +0530

    improving the lists design

--- to_do_list.js
+++ to_do_list.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', function() {
         let element = document.createElement('div');
         let del = document.createElement('button');
 
-       
         element.innerHTML = txt;
 
         del.innerHTML = 'Delete';
@@ -32,16 +31,16 @@ document.addEventListener('DOMContentLoaded', function() {
         del.style.borderRadius = '8px';
         del.style.textAlign = 'center';
         del.style.justifyContent = 'center';
-        del.style.paddingTop = '6px';
-        del.style.paddingLeft='7px';
-        del.style.paddingBottom = '6px';
-        del.style.paddingRight='7px';
-        del.style.fontWeight='bold';
+        del.style.paddingTop = '5px';
+        del.style.paddingLeft = '7px';
+        del.style.paddingBottom = '5px';
+        del.style.paddingRight = '7px';
+        del.style.fontWeight = 'bold';
         del.style.cursor = 'pointer';
 
         element_with_delete.style.display = 'flex';
-        element_with_delete.style.alignItems='center';
-        element_with_delete.style.justifyContent='center';
+        element_with_delete.style.alignItems = 'center';
+        element_with_delete.style.justifyContent = 'center';
         element_with_delete.style.marginBottom = '30px';
         element.style.marginRight = '50px';
         element_with_delete.appendChild(element);
@@ -49,29 +48,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let new_id = 'a' + id;
         element_with_delete.classList.add(new_id);
-        
+
         del.setAttribute('id', new_id);
         item.appendChild(element_with_delete);
 
-        var items=document.querySelectorAll('.delete')
-        items.forEach((item)=>{
-        item.addEventListener('click',(e)=>{
-        var parent=item.parentNode;
-        var grand_parent=parent.parentNode;
-        grand_parent.removeChild(parent);
-        parent.innerHTML=null;
-        let idd=item.getAttribute('id');
-        let new_idd=idd.slice(1);
-        console.log(new_idd);
-        delete to_do_list[new_idd];
-    })})
+        let items = document.querySelectorAll('.delete');
+        items.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                let parent = item.parentNode;
+                let grand_parent = parent.parentNode;
+                grand_parent.removeChild(parent);
+                parent.innerHTML = null;
+                let idd = item.getAttribute('id');
+                let new_idd = idd.slice(1);
+                delete to_do_list[new_idd];
+                display();
+            });
+        });
+    }
+
+    function to_do() {
+        let item = document.createElement('h2');
+        item.innerText = 'My List';
+        let par = document.querySelector('.display_list');
+        par.appendChild(item);
+    }
+
+    function empty() {
+        let item = document.createElement('h3');
+        item.innerText = 'Your to do list is empty, add some items to the list.';
+        let par = document.querySelector('.display_list');
+        par.appendChild(item);
+        item.style.color = '#ee3425';
     }
 
     function display() {
         let item = document.querySelector('.display_list');
         item.innerHTML = null;
+        let num_of_items = 0;
+        to_do();
         for (let key in to_do_list) {
-            display_on_webpage(to_do_list[key], key);
+            num_of_items++;
+        }
+        if (num_of_items === 0) {
+            empty();
+        } else {
+            for (let key in to_do_list) {
+                display_on_webpage(to_do_list[key], key);
+            }
         }
     }
 
@@ -80,6 +104,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let txt = item.value;
         item.value = null;
         add(txt);
+        display();
     });
 
     document.querySelector('.display').addEventListener('click', (e) => {
@@ -92,14 +117,12 @@ document.addEventListener('DOMContentLoaded', function() {
             let txt = item.value;
             item.value = null;
             add(txt);
+            display();
         }
     });
-
-    
 
     document.querySelector('.hide').addEventListener('click', (e) => {
         let item = document.querySelector('.display_list');
         item.innerHTML = null;
     });
-    
 });
