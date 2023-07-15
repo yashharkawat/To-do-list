e66b34250dec9a29546869b2c3d20b8198b62fa9
Bikash Sahoo <Yashharkawat2000@gmail.com>
2023-07-15 18:40:50 +0530

    bonus done and also used local storage

--- to_do_list.js
+++ to_do_list.js
@@ -1,5 +1,6 @@
 let to_do_list = [];
 let id = 0;
+
 fetch('https://jsonplaceholder.typicode.com/todos')
   .then((response) => {
     if (!response.ok) {
@@ -8,126 +9,157 @@ fetch('https://jsonplaceholder.typicode.com/todos')
     return response.json();
   })
   .then((data) => {
-    // Process the received data
-    data.forEach((item)=>{
-        to_do_list.push({txt:`• ${item.title}`,id:id});
+    if (localStorage.length === 0) {
+      data.forEach((item) => {
+        to_do_list.push({ txt: item.title, id: id });
         id++;
-        
-    })
+      });
+      localStorage.setItem('to_do_list', JSON.stringify(to_do_list));
+      localStorage.setItem('id', id);
+    } else {
+      id = localStorage.getItem('id');
+      to_do_list = JSON.parse(localStorage.getItem('to_do_list'));
+    }
   })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch request
+  .catch((error) => {
     console.log('Error:', error.message);
   });
 
-document.addEventListener('DOMContentLoaded', function() {
-    function add(txt) {
-        if (txt.length == 0) return;
+document.addEventListener('DOMContentLoaded', () => {
+  function add(txt) {
+    if (txt.length === 0) return;
+    to_do_list.push({ txt: txt, id: id });
+    id++;
+    localStorage.setItem('to_do_list', JSON.stringify(to_do_list));
+    localStorage.setItem('id', id);
+  }
 
-        let text='• ' + txt;
-        to_do_list.push({txt:text,id:id})
-        id++;
-    }
-    function display_on_webpage(txt, id) {
-        let item = document.querySelector('.display_list');
+  function display_on_webpage(txt, id) {
+    let item = document.querySelector('.display_list');
+    let element_with_delete = document.createElement('ul');
+    let element = document.createElement('li');
+    let del = document.createElement('button');
 
-        let element_with_delete = document.createElement('div');
-        let element = document.createElement('div');
-        let del = document.createElement('button');
+    element.innerHTML = txt;
 
-        element.innerHTML = txt;
+    del.innerHTML = 'Delete';
+    del.setAttribute('id', id);
+    del.classList.add('delete');
+    
+    
+    element.style.marginRight = '50px';
+    element_with_delete.appendChild(element);
+    element_with_delete.appendChild(del);
 
-        del.innerHTML = 'Delete';
-        del.setAttribute('id', id);
-        del.classList.add('delete');
+    let new_id = 'delete_' + id;
+    element_with_delete.classList.add(new_id);
+    element_with_delete.classList.add("to_do_element");
 
-        del.style.backgroundColor = '#ee3425';
-        del.style.color = 'white';
-        del.style.border = '1px transparent';
-        del.style.borderRadius = '8px';
-        del.style.textAlign = 'center';
-        del.style.justifyContent = 'center';
-        del.style.paddingTop = '5px';
-        del.style.paddingLeft = '7px';
-        del.style.paddingBottom = '5px';
-        del.style.paddingRight = '7px';
-        del.style.fontWeight = 'bold';
-        del.style.cursor = 'pointer';
+    del.setAttribute('id', new_id);
+    item.appendChild(element_with_delete);
 
-        element_with_delete.style.display = 'flex';
-        element_with_delete.style.alignItems = 'center';
-        element_with_delete.style.justifyContent = 'center';
-        element_with_delete.style.marginBottom = '30px';
-        element.style.marginRight = '50px';
-        element_with_delete.appendChild(element);
-        element_with_delete.appendChild(del);
-
-        let new_id = 'delete_' + id;
-        element_with_delete.classList.add(new_id);
-
-        del.setAttribute('id', new_id);
-        item.appendChild(element_with_delete);
-
-        let items = document.querySelectorAll('.delete');
-        items.forEach((item) => {
-            item.addEventListener('click', (e) => {
-                let parent = item.parentNode;
-                let grand_parent = parent.parentNode;
-                grand_parent.removeChild(parent);
-                parent.innerHTML = null;
-                let idd = item.getAttribute('id');
-                let new_idd = idd.slice(7);
-                to_do_list[new_idd].txt=-1;
-                display();
-            });
-        });
-    }
-
-    function list_heading() {
-        let item = document.createElement('h2');
-        item.innerText = 'My List';
-        let par = document.querySelector('.display_list');
-        par.appendChild(item);
-    }
-
-
-    function display() {
-        let item = document.querySelector('.display_list');
-        item.innerHTML = null;
-        list_heading();
-        to_do_list.forEach((item)=>{
-            if(item.txt!=-1){
-                display_on_webpage(item.txt, item.id);
-            }
-        })
-    }
-
-    document.querySelector('.save_button').addEventListener('click', () => {
-        let item = document.querySelector('.save');
-        let txt = item.value;
-        item.value = null;
-        add(txt);
-        if(txt.length>0) display();
-    });
-
-    document.querySelector('.display').addEventListener('click', (e) => {
+    let items = document.querySelectorAll('.delete');
+    items.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        let parent = item.parentNode;
+        let grand_parent = parent.parentNode;
+        grand_parent.removeChild(parent);
+        parent.innerHTML = null;
+        let idd = item.getAttribute('id');
+        let new_idd = idd.slice(7);
+        to_do_list[new_idd].txt = -1;
+        localStorage.setItem('to_do_list', JSON.stringify(to_do_list));
         display();
+      });
     });
+    let element_id = 'element' + id;
+    element.setAttribute('id', element_id);
+    element.addEventListener('click', (e) => {
+      let input_id = e.target.getAttribute('id');
+      convert_to_input(input_id);
+    });
+  }
+
+  function convert_to_input(element_id) {
+    let arr_id = element_id.slice(7);
+    let text_element = document.getElementById(element_id);
+    let text_content = text_element.textContent;
+    let text_element_parent = text_element.parentNode;
+
+    let parent = text_element_parent.parentNode;
+
+    let input_element = document.createElement('input');
+    input_element.value = text_content;
+
+    let save_button = document.createElement('button');
+    save_button.textContent = 'Save';
+    save_button.classList.add('save_input');
+
+    let input = document.createElement('div');
+    input.append(input_element, save_button);
+    input.classList.add('element_input');
+
+    parent.replaceChild(input, text_element_parent);
+    input_element.focus();
 
     document.addEventListener('keypress', (e) => {
-        if (e.key == 'Enter') {
-            let item = document.querySelector('.save');
-            let txt = item.value;
-            item.value = null;
-            add(txt);
-            if(txt.length>0) display();
-        }
+      if (e.key === 'Enter') {
+        text_element.textContent = input_element.value;
+        parent.replaceChild(text_element_parent, input);
+        to_do_list[arr_id].txt = input_element.value;
+        localStorage.setItem('to_do_list', JSON.stringify(to_do_list));
+      }
     });
+    save_button.addEventListener('click', (e) => {
+      text_element.textContent = input_element.value;
+      parent.replaceChild(text_element_parent, input);
+      to_do_list[arr_id].txt = input_element.value;
+      localStorage.setItem('to_do_list', JSON.stringify(to_do_list));
+    });
+  }
 
-    document.querySelector('.hide').addEventListener('click', (e) => {
-        let item = document.querySelector('.display_list');
-        item.innerHTML = null;
+  function list_heading() {
+    let item = document.createElement('h2');
+    item.innerText = 'My List';
+    let par = document.querySelector('.display_list');
+    par.appendChild(item);
+  }
+
+  function display() {
+    let item = document.querySelector('.display_list');
+    item.innerHTML = null;
+    list_heading();
+    to_do_list.forEach((item) => {
+      if (item.txt !== -1) {
+        display_on_webpage(item.txt, item.id);
+      }
     });
+  }
+
+  document.querySelector('.save_button').addEventListener('click', () => {
+    let item = document.querySelector('.save');
+    let txt = item.value;
+    item.value = null;
+    add(txt);
+    if (txt.length > 0) display();
+  });
+
+  document.querySelector('.display').addEventListener('click', (e) => {
+    display();
+  });
+
+  document.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      let item = document.querySelector('.save');
+      let txt = item.value;
+      item.value = null;
+      add(txt);
+      if (txt.length > 0) display();
+    }
+  });
+
+  document.querySelector('.hide').addEventListener('click', (e) => {
+    let item = document.querySelector('.display_list');
+    item.innerHTML = null;
+  });
 });
-
-
